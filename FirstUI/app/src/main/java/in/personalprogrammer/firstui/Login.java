bcb385a541335e7d094857317ad4ecd6a0190c16
mohitcs <mohitcs@live.com>
2017-11-02 18:54:44 +0530

    Login Design
    Output.jpg

---
+++ FirstUI/app/src/main/java/in/personalprogrammer/firstui/Login.java
@@ -0,0 +1,17 @@
+package in.personalprogrammer.firstui;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Mohit on 27/10/2017.
+ */
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_layout);
+    }
+}
